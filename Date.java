024af37d4aa0018cb6c8c481fff8312f73ebe02e
kang-7976 <pointo0987654321@163.com>
2026-06-30 23:19:49 +0800

    hello java

---
+++ Date.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Main {
+  static Date dt = new Date();
+
+  public static void main(String[] args) {
+    zcx();
+  }
+  static void zcx() {
+    System.out.println(dt);
+  }
+}
+
